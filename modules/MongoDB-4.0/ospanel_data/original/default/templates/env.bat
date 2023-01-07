7c2916807bccc8b48f198af74548fb2600309f9c
OSPanel <93676052+OSPanel@users.noreply.github.com>
2023-01-07 07:12:51 +0300

    Update env.bat

--- modules/MongoDB-4.0/ospanel_data/original/default/templates/env.bat
+++ modules/MongoDB-4.0/ospanel_data/original/default/templates/env.bat
@@ -1,9 +1,9 @@
 {environment}
 if /i not "{terminal_codepage}"=="" chcp {terminal_codepage}>nul
 if /i not "{terminal_codepage}"=="" if /i not "%1"=="shell" set "OSP_TERMINAL_CODEPAGE={terminal_codepage}"
-if /i not "%1"=="add" set "OSP_ACTIVE_ENV={module_name}"
-if /i "%1"=="add" set "OSP_ACTIVE_ENV=%OSP_ACTIVE_ENV% + {module_name}"
-if /i not "%3"=="silent" if not exist "{root_dir}\temp\{module_name}.lock" echo: & echo  %ESC%[33m{lang_17}: {module_name} {lang_172}%ESC%[0m
+if /i not "%1"=="add" set "OSP_ACTIVE_ENV=%ESC%[94m{module_name}%ESC%[0m" & set "OSP_ACTIVE_ENV_T={module_name}"
+if /i "%1"=="add" set "OSP_ACTIVE_ENV=%ESC%[94m%OSP_ACTIVE_ENV%%ESC%[0m + %ESC%[94m{module_name}%ESC%[0m" & set "OSP_ACTIVE_ENV_T=%OSP_ACTIVE_ENV_T% + {module_name}"
+if /i not "%3"=="silent" if not exist "{root_dir}\temp\{module_name}.lock" echo: & echo  %ESC%[93m{lang_17}: {module_name} {lang_172}%ESC%[0m
 if /i not "%1"=="shell" echo: & echo  {lang_52}: %OSP_ACTIVE_ENV%
-if /i not "%3"=="silent" if /i not "%1"=="shell" TITLE Open Server Panel ^| %OSP_ACTIVE_ENV%
+if /i not "%3"=="silent" if /i not "%1"=="shell" TITLE Open Server Panel ^| %OSP_ACTIVE_ENV_T%
 if /i "%1"=="shell" echo:
