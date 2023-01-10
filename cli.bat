7ca56fa1e4d5894b9a15ce751deeed6faa17debd
OSPanel <93676052+OSPanel@users.noreply.github.com>
2023-01-10 06:31:07 +0300

    Update cli.bat

--- cli.bat
+++ cli.bat
@@ -1,4 +1,3 @@
-@echo off
-if not exist "%~dp0temp\OSPanel.lock" exit /b 1
-if not exist "%~dp0bin\osp.bat" exit /b 1
-start "" "%~dp0bin\osp.bat" reset init
+@if not exist "%~dp0temp\OSPanel.lock" exit /b 1
+@if not exist "%~dp0bin\osp.bat" exit /b 1
+@"%~dp0bin\osp.bat" reset init
