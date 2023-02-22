138769633ff0b2513a69f0c5c6575a7791eebf2e
OSPanel <93676052+OSPanel@users.noreply.github.com>
2023-02-22 21:47:48 +0300

    Update env.bat

--- resources/cmd/env.bat
+++ resources/cmd/env.bat
@@ -1,5 +1,4 @@
 {environment}
-if /i not "{terminal_codepage}"=="" chcp {terminal_codepage}>nul
 if /i not "{terminal_codepage}"=="" if /i not "%1"=="shell" set "OSP_TERMINAL_CODEPAGE={terminal_codepage}"
 if /i not "%1"=="add" set "OSP_ACTIVE_ENV={module_name}"
 if /i "%1"=="add" set "OSP_ACTIVE_ENV=%OSP_ACTIVE_ENV% + {module_name}"
