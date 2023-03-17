f5dbda1f5c04e448dd8223735bc9cba785125747
OSPanel <93676052+OSPanel@users.noreply.github.com>
2023-03-18 00:02:50 +0300

    Update env.bat

--- resources/cmd/env.bat
+++ resources/cmd/env.bat
@@ -2,6 +2,6 @@
 if /i not "{terminal_codepage}"=="" if /i not "%1"=="shell" set "OSP_CODEPAGE={terminal_codepage}" & if defined OSP_TMP_CODEPAGE set "OSP_TMP_CODEPAGE={terminal_codepage}"
 if /i not "%1"=="add" set "OSP_ACTIVE_ENV={module_name}"
 if /i "%1"=="add" set "OSP_ACTIVE_ENV=%OSP_ACTIVE_ENV% + {module_name}"
-if not defined OSP_PSV if not exist "{root_dir}\temp\{module_name}.lock" echo: & echo  %ESC%[93m{lang_17}: {module_name} {lang_172}%ESC%[0m
-if /i not "%1"=="shell" if /i not "%3"=="silent" echo: & echo  {lang_52}: %OSP_ACTIVE_ENV%
+if not defined OSP_PSV if not exist "{root_dir}\temp\{module_name}.lock" echo: & echo %ESC%[93m{lang_17}: {module_name} {lang_172}%ESC%[0m
+if /i not "%1"=="shell" if /i not "%3"=="silent" echo: & echo {lang_52}: %OSP_ACTIVE_ENV%
 if /i not "%1"=="shell" TITLE OSPanel ^| %OSP_ACTIVE_ENV%
