3fd30e61243ad677a64917d5eceff6cef62634fb
OSPanel <93676052+OSPanel@users.noreply.github.com>
2023-03-20 02:41:48 +0300

    Update osp.bat

--- modules/ControlPanel/ospanel_data/original/default/templates/osp.bat
+++ modules/ControlPanel/ospanel_data/original/default/templates/osp.bat
@@ -80,10 +80,10 @@ echo                             {lang_91}
 echo                             {lang_92}
 echo info                        {lang_93}
 echo project ^<DOMAIN^>  [silent]  {lang_186}
+echo                             {lang_168}
 echo                             {lang_189}
 echo reset   [init]              {lang_94}
 echo                             {lang_167}
-echo                             {lang_168}
 echo set     ^<MODULE^>            {lang_95}
 echo                             {lang_165}
 echo                             {lang_166}
