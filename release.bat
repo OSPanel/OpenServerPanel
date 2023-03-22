2c835151250e9fac65d4a632ef79be04e5057f6c
OSPanel <93676052+OSPanel@users.noreply.github.com>
2023-03-22 22:05:12 +0300

    Update release.bat

--- release.bat
+++ release.bat
@@ -46,35 +46,5 @@ xcopy "%~dp0resources\cmd\shell.bat" "%~dp0modules\Redis-7.0\ospanel_data\origin
 for /d %%D in ("%~dp0modules\*") do xcopy "%~dp0resources\cmd\env.bat" "%%D\ospanel_data\original\default\templates\" /y
 del "%~dp0modules\ControlPanel\ospanel_data\original\default\templates\env.bat"
 for /d %%D in ("%~dp0modules\*") do robocopy "%%D\ospanel_data\original" "%~dp0config\%%~nxD" /UNICODE /DCOPY:DAT /COPY:DAT /TIMFIX /MIR /J /ETA /IM /MT:32 /R:3 /W:3
-for /d %%D in ("%~dp0generate\config\*") do robocopy "%%D" "%~dp0config\%%~nxD" /UNICODE /DCOPY:DAT /COPY:DAT /TIMFIX /E /J /ETA /IM /MT:32 /R:3 /W:3
-xcopy "%~dp0resources\cmd\shell.bat" "%~dp0config\MariaDB-10.1\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\shell.bat" "%~dp0config\MariaDB-10.2\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\shell.bat" "%~dp0config\MariaDB-10.3\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\shell.bat" "%~dp0config\MariaDB-10.4\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\shell.bat" "%~dp0config\MariaDB-10.5\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\shell.bat" "%~dp0config\MariaDB-10.6\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\shell.bat" "%~dp0config\MariaDB-10.7\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\shell.bat" "%~dp0config\MariaDB-10.8\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\shell.bat" "%~dp0config\MariaDB-10.9\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\shell.bat" "%~dp0config\MariaDB-10.10\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\shell.bat" "%~dp0config\MariaDB-10.11\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\env.bat" "%~dp0config\MariaDB-10.1\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\env.bat" "%~dp0config\MariaDB-10.2\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\env.bat" "%~dp0config\MariaDB-10.3\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\env.bat" "%~dp0config\MariaDB-10.4\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\env.bat" "%~dp0config\MariaDB-10.5\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\env.bat" "%~dp0config\MariaDB-10.6\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\env.bat" "%~dp0config\MariaDB-10.7\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\env.bat" "%~dp0config\MariaDB-10.8\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\env.bat" "%~dp0config\MariaDB-10.9\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\env.bat" "%~dp0config\MariaDB-10.10\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\env.bat" "%~dp0config\MariaDB-10.11\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\shell.bat" "%~dp0config\MySQL-5.5\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\shell.bat" "%~dp0config\MySQL-5.6\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\shell.bat" "%~dp0config\MySQL-5.7\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\shell.bat" "%~dp0config\MySQL-8.0\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\env.bat" "%~dp0config\MySQL-5.5\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\env.bat" "%~dp0config\MySQL-5.6\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\env.bat" "%~dp0config\MySQL-5.7\initdb\templates\" /y
-xcopy "%~dp0resources\cmd\env.bat" "%~dp0config\MySQL-8.0\initdb\templates\" /y
 PAUSE
+echo on
