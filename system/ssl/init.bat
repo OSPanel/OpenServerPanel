1ec56059d460e98e32b25963f000bb434cd9a7c0
OSPanel <93676052+OSPanel@users.noreply.github.com>
2023-04-26 02:41:08 +0300

    Update init.bat

--- system/ssl/init.bat
+++ system/ssl/init.bat
@@ -11,4 +11,4 @@ set "OPENSSL_CONF=%~dp0openssl.cnf"
 "%~dp0openssl.exe" ecparam -genkey -name prime256v1 -out "%~dp0..\..\user\ssl\default\cert.key"
 "%~dp0openssl.exe" req -new -sha256 -key "%~dp0..\..\user\ssl\default\cert.key" -out "%~dp0..\..\user\ssl\default\cert.csr" -subj "/emailAddress=root@ospanel.local/C=EU/stateOrProvinceName=Local Network/O=Open Server Panel/CN=Default Certificate"
 "%~dp0openssl.exe" x509 -req -sha256 -days 3650 -in "%~dp0..\..\user\ssl\default\cert.csr" -signkey "%~dp0..\..\user\ssl\default\cert.key" -out "%~dp0..\..\user\ssl\default\cert.crt"
-
+if /i "%1"=="addstore" call "%~dp0add_root_to_certstore.bat"
