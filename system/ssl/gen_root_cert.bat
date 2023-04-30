c951ba3d9fb0984b22e7fb48974dbe952aa3b9b0
OSPanel <93676052+OSPanel@users.noreply.github.com>
2023-04-30 19:21:20 +0300

    Create gen_root_cert.bat

---
+++ system/ssl/gen_root_cert.bat
@@ -0,0 +1,8 @@
+echo:
+setlocal
+set "OPENSSL_CONF=%~dp0openssl.cnf"
+if not exist "%~dp0..\..\data\ssl\root\" mkdir "%~dp0..\..\data\ssl\root"
+"%~dp0openssl.exe" ecparam -genkey -name prime256v1 -out "%~dp0..\..\data\ssl\root\cert.key"
+"%~dp0openssl.exe" req -x509 -new -sha256 -key "%~dp0..\..\data\ssl\root\cert.key" -days 3650 -out "%~dp0..\..\data\ssl\root\cert.crt" -subj "/emailAddress=root@ospanel.local/C=EU/stateOrProvinceName=Local Network/O=Open Server Panel/CN=Open Server Panel"
+if /i "%1"=="addstore" call "%~dp0add_root_to_certstore.bat"
+endlocal
