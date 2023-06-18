85700f80a540821347d60d9885ead3b138ef447f
OSPanel <93676052+OSPanel@users.noreply.github.com>
2023-06-18 05:24:02 +0300

    Update genlicense.bat

--- generate/genlicense.bat
+++ generate/genlicense.bat
@@ -1,10 +1,11 @@
 @echo off
+set "TMP_FILE=%~dp0tmp_license.txt"
+set "HEAD_FILE=%~dp0LICENSE_HEAD"
+set "RESULT_FILE=%~dp0..\licenses\licenses\Open Server Panel\LICENSE"
+set "MASTER_FILE=%~dp0..\LICENSE"
 cd ..\licenses
-setlocal
-set "TMP_FILE=tmp_license.txt"
-del /Q "licenses\Open Server Panel\LICENSE"
-del /Q %TMP_FILE%
-forfiles /S /C "cmd /c if @isdir==FALSE echo: >> %TMP_FILE% & echo: >> %TMP_FILE% & echo *** LICENSE FILE @RELPATH: >> %TMP_FILE% & echo: >> %TMP_FILE% & echo: >> %TMP_FILE% & type @PATH >> %TMP_FILE%"
-copy ..\generate\LICENSE_HEAD + %TMP_FILE% "licenses\Open Server Panel\LICENSE"
-del /Q %TMP_FILE%
-endlocal
+del /Q "%RESULT_FILE%"
+forfiles /S /C "cmd /c if @isdir==FALSE echo: & echo: & echo *** LICENSE FILE @RELPATH: & echo: & echo: & type @PATH" >> %TMP_FILE%
+copy "%HEAD_FILE%" + "%TMP_FILE%" "%RESULT_FILE%"
+del /Q "%TMP_FILE%"
+copy "%RESULT_FILE%" "%MASTER_FILE%"
