13e7051ffb6e2214bad6858a25f8f7a1b6497f4a
OSPanel <93676052+OSPanel@users.noreply.github.com>
2023-06-18 08:57:59 +0300

    Update genlicense.bat

--- generate/genlicense.bat
+++ generate/genlicense.bat
@@ -4,7 +4,7 @@ set "HEAD_FILE=%~dp0LICENSE_HEAD"
 set "RESULT_FILE=%~dp0..\licenses\licenses\Open Server Panel\LICENSE"
 set "MASTER_FILE=%~dp0..\LICENSE"
 cd ..\licenses
-del /Q "%RESULT_FILE%"
+del /Q "%RESULT_FILE%" >nul 2>nul
 forfiles /S /C "cmd /c if @isdir==FALSE echo: & echo: & echo LICENSE FILE @RELPATH: & echo: & echo: & type @PATH" >> %TMP_FILE%
 copy "%HEAD_FILE%" + "%TMP_FILE%" "%RESULT_FILE%"
 del /Q "%TMP_FILE%"
