6552f46f55d97c2dd59df5f14ee6f2c0abd18bdb
OSPanel <93676052+OSPanel@users.noreply.github.com>
2023-06-19 17:17:33 +0300

    Delete genconfigs.bat

--- generate/genconfigs.bat
+++
@@ -1,7 +0,0 @@
-:: --------------------------------------------------------------------------------
-:: OPEN SERVER PANEL | CONFIGURATION FILE GENERATOR
-:: --------------------------------------------------------------------------------
-@echo off
-for /d %%D in ("%~dp0..\modules\*") do robocopy "%%D\ospanel_data\original" "%~dp0..\config\%%~nxD" /UNICODE /DCOPY:DAT /COPY:DAT /TIMFIX /MIR /J /ETA /IM /MT:32 /R:3 /W:3
-echo on
-@PAUSE
