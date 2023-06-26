743feede679a388b4506a85f27521ca041d54f18
OSPanel <93676052+OSPanel@users.noreply.github.com>
2023-06-26 09:17:19 +0300

    Update osp.bat

--- system/cmd/osp.bat
+++ system/cmd/osp.bat
@@ -1,2 +1,2 @@
-@if not exist "%~dp0..\..\bin\osp.bat" @echo: & @echo First you need to run the Open Server Panel! & @exit /b 1
+@if not exist "%~dp0..\..\bin\osp.bat" @echo: & @echo First you need to run the Open Server Panel! & @echo Type the command "ospanel" and press Enter... & @exit /b 1
 @"%~dp0..\..\bin\osp.bat" %*
