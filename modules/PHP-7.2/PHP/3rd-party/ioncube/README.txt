1be7ea53295cfb9c33164ef742c5c19fc22b51a5
OSPanel <93676052+OSPanel@users.noreply.github.com>
2024-05-10 02:45:21 +0300

    Add ioncube

---
+++ modules/PHP-7.2/PHP/3rd-party/ioncube/README.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6a9a9ce1da35a225133e667154a122aa1fb37204c95547ccc7bc9c43dee4cd27
+size 4070
---
+++ modules/PHP-8.2/PHP/3rd-party/ioncube/loader-wizard.php
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e807e73ec65f3ca80098cde92dcffa5d1bac10a2b7a75228de9e309ef3321a5a
+size 181104
