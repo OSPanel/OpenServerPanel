319df510b8dfa3bb3c1c54ad435ca608848a3a00
OSPanel <93676052+OSPanel@users.noreply.github.com>
2025-04-09 23:01:13 +0300

    Update PHP data

--- generate/cc.bat
+++ generate/cc.bat
@@ -12,6 +12,8 @@ for %%V in (7.2 7.3 7.4 8.0 8.1 8.2 8.3 8.4) do (
     set "TARGET1=..\modules\PHP-%%V\ospanel_data\default_data\composer"
     if exist "!TARGET1!" (
         copy /Y "%SOURCE%" "!TARGET1!\composer.phar" >nul
+        copy /Y "%SOURCE%" "!TARGET1!\keys.tags.pub" >nul
+        copy /Y "%SOURCE%" "!TARGET1!\keys.dev.pub" >nul
     ) else (
         echo !TARGET1! not found!
     )
@@ -19,6 +21,8 @@ for %%V in (7.2 7.3 7.4 8.0 8.1 8.2 8.3 8.4) do (
     set "TARGET2=..\modules\PHP-%%V-FCGI\ospanel_data\default_data\composer"
     if exist "!TARGET2!" (
         copy /Y "%SOURCE%" "!TARGET2!\composer.phar" >nul
+        copy /Y "%SOURCE%" "!TARGET2!\keys.tags.pub" >nul
+        copy /Y "%SOURCE%" "!TARGET2!\keys.dev.pub" >nul
     ) else (
         echo !TARGET2! not found!
     )
