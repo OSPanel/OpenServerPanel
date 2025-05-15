970fbca0996df1fa44dbd1ddda9b5a9a47917eae
OSPanel <93676052+OSPanel@users.noreply.github.com>
2025-05-15 04:21:26 +0300

    Update Smtp4dev

--- modules/Smtp4dev/wwwroot/assets/index-CGw1VrTm.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6076d8988573bd4b059d6926457d38f9efccbe2940ab9d840eed7a5a01b74fc0
-size 4947779
---
+++ modules/Smtp4dev/wwwroot/assets/index-VWZmRHsb.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:13132e99ed951e33a5ae51d3c7f59690d3e90f90d9f6a051c396a2a1bf3d8273
+size 5024041
