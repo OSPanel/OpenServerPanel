a89e6b9edaa87b7068c0e524b542bf81a63b91da
OSPanel <93676052+OSPanel@users.noreply.github.com>
2025-05-15 20:31:36 +0300

    Add InstantClient addons

---
+++ addons/InstantClient-11.2/glogin.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4ade3469b03790d8390dbb9fabf5ffe313c2ead803ab5f02e03426eb9e3dfc16
+size 368
---
+++ addons/InstantClient-12.2/glogin.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c0002e6ca4a8409d1f7f9c6533858631c18926ce11d219742bca242af9bf934c
+size 342
